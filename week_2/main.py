4abdb1818769631f42c3f722ed1e5d5729e3c03e
Ho Ni Xuan (Natalie) <natalieho061@gmail.com>
2026-05-20 17:02:37 +0800

    feat: Add data tagging

--- week_2/main.py
+++ week_2/main.py
@@ -1,5 +1,21 @@
+import sys
+from pathlib import Path 
+from src.prompt_model import prompt_model
+from src.tag_data import tag_data
+
+DB = Path("data/jobs.db")
+
+def test_prompt():
+    if len(sys.argv) != 3:
+        print(f'Usage: uv run prompt_model.py <model> <prompt>')
+    else:        
+        model = sys.argv[1]
+        prompt = sys.argv[2]
+        prompt_model(model, prompt)
+
+
 def main():
-    print("Hello from week-2!")
+    tag_data(DB)
 
 
 if __name__ == "__main__":
